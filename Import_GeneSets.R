c8e16d6feaf9daf3f5272cb9cbbbbf370919990d
ellalamont <elamont@uw.edu>
2026-02-03 12:24:40 -0800

    minor commenting

--- Import_GeneSets.R
+++ Import_GeneSets.R
@@ -56,42 +56,47 @@ allGeneSetList <- lapply(allGeneSetList, function(gset) {
 # allGeneSets <- split(EllaGeneSets_2026.01.30$Gene, EllaGeneSets_2026.01.30$GeneSet)
 # save(allGeneSets, file = "Data/GeneSet_Data/EllaGeneSets_2026.01.30.rda")
 
+# 2/3/25: New gene sets for lance
+# LRFGeneSets_2026.02.03 <- read.csv("Data/GeneSet_Data/LRF_GeneSets_2026.02.03.csv")
+# allGeneSets <- split(LRFGeneSets_2026.02.03$Gene, LRFGeneSets_2026.02.03$GeneSet)
+# save(allGeneSets, file = "Data/GeneSet_Data/LRFGeneSets_2026.02.03.rda")
+
 
 ###########################################################
 ############ iMODULONS: MAKE LISTS OF GROUPS ##############
 
-CentralCarbon_iModulons <- c("Peptidoglycan Biosynthesis", "Central Carbon Metabolism", "Fumarate Reductase", "PrpR", "BkaR", "Nicotinate Metabolism") # Needs the \\ to detect it
-CentralCarbon_iModulons_pattern <- str_c(CentralCarbon_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
-
-AminoAcid_iModulons <- c("GroEL-GroES Complex", "Leucine Related", "LysG", "ArgR") # Needs the \\ to detect it
-AminoAcid_iModulons_pattern <- str_c(AminoAcid_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
-
-NucleicAcid_iModulons <- c("PyrR", "Rv0135\\+Rv1019", "Nucleic Acid Hydrolysis") # Needs the \\ to detect it
-NucleicAcid_iModulons_pattern <- str_c(NucleicAcid_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
-
-FattyAcid.Cholesterol_iModulons <- c("Fatty Acid Biosynthesis", "KstR2", "Mycofactocin Synthesis Pathway", "FasR", "Polyketide Synthase Complex", "Rv0681") # Needs the \\ to detect it
-FattyAcid.Cholesterol_iModulons_pattern <- str_c(FattyAcid.Cholesterol_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
-
-Metal_iModulons <- c("RicR", "IdeR", "M-box", "Zur", "Hpt-2b Induced") # Needs the \\ to detect it
-Metal_iModulons_pattern <- str_c(Metal_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
-
-SulfurMetabolism_iModulons <- c("Sulfur Metabolism") # Needs the \\ to detect it
-SulfurMetabolism_iModulons_pattern <- str_c(SulfurMetabolism_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
-
-Growth_iModulons <- c("Positive Regulation of Growth") # Needs the \\ to detect it
-Growth_iModulons_pattern <- str_c(Growth_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
-
-Redox_iModulons <- c("DevR-1", "WhiB4", "DevR-2", "WhiB1", "WhiB4/IdeR", "Rv1828/SigH", "Rv1776c\\+WhiB4", "VirS", "WhiB6") # Needs the \\ to detect it
-Redox_iModulons_pattern <- str_c(Redox_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
-
-AcidStress_iModulons <- c("MarR") # Needs the \\ to detect it
-AcidStress_iModulons_pattern <- str_c(AcidStress_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
-
-Antibiotic_iModulons <- c("Lsr2", "Blal", "Rv0078\\+Rv2034", "WhiB7", "IniR") # Needs the \\ to detect it
-Antibiotic_iModulons_pattern <- str_c(Antibiotic_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
-
-Virulence.Persistence_iModulons <- c("Rv0576", "Mce1R", "SigH", "PhoP", "Mce3R", "MprA", "PDIM\\;PGL Synthesis", "Rv2488c", "SigC", "SigD", "MbcA\\+Rv3249c\\+Rv3066", "SigK") # Needs the \\ to detect it
-Virulence.Persistence_iModulons_pattern <- str_c(Virulence.Persistence_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
+# CentralCarbon_iModulons <- c("Peptidoglycan Biosynthesis", "Central Carbon Metabolism", "Fumarate Reductase", "PrpR", "BkaR", "Nicotinate Metabolism") # Needs the \\ to detect it
+# CentralCarbon_iModulons_pattern <- str_c(CentralCarbon_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
+# 
+# AminoAcid_iModulons <- c("GroEL-GroES Complex", "Leucine Related", "LysG", "ArgR") # Needs the \\ to detect it
+# AminoAcid_iModulons_pattern <- str_c(AminoAcid_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
+# 
+# NucleicAcid_iModulons <- c("PyrR", "Rv0135\\+Rv1019", "Nucleic Acid Hydrolysis") # Needs the \\ to detect it
+# NucleicAcid_iModulons_pattern <- str_c(NucleicAcid_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
+# 
+# FattyAcid.Cholesterol_iModulons <- c("Fatty Acid Biosynthesis", "KstR2", "Mycofactocin Synthesis Pathway", "FasR", "Polyketide Synthase Complex", "Rv0681") # Needs the \\ to detect it
+# FattyAcid.Cholesterol_iModulons_pattern <- str_c(FattyAcid.Cholesterol_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
+# 
+# Metal_iModulons <- c("RicR", "IdeR", "M-box", "Zur", "Hpt-2b Induced") # Needs the \\ to detect it
+# Metal_iModulons_pattern <- str_c(Metal_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
+# 
+# SulfurMetabolism_iModulons <- c("Sulfur Metabolism") # Needs the \\ to detect it
+# SulfurMetabolism_iModulons_pattern <- str_c(SulfurMetabolism_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
+# 
+# Growth_iModulons <- c("Positive Regulation of Growth") # Needs the \\ to detect it
+# Growth_iModulons_pattern <- str_c(Growth_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
+# 
+# Redox_iModulons <- c("DevR-1", "WhiB4", "DevR-2", "WhiB1", "WhiB4/IdeR", "Rv1828/SigH", "Rv1776c\\+WhiB4", "VirS", "WhiB6") # Needs the \\ to detect it
+# Redox_iModulons_pattern <- str_c(Redox_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
+# 
+# AcidStress_iModulons <- c("MarR") # Needs the \\ to detect it
+# AcidStress_iModulons_pattern <- str_c(AcidStress_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
+# 
+# Antibiotic_iModulons <- c("Lsr2", "Blal", "Rv0078\\+Rv2034", "WhiB7", "IniR") # Needs the \\ to detect it
+# Antibiotic_iModulons_pattern <- str_c(Antibiotic_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
+# 
+# Virulence.Persistence_iModulons <- c("Rv0576", "Mce1R", "SigH", "PhoP", "Mce3R", "MprA", "PDIM\\;PGL Synthesis", "Rv2488c", "SigC", "SigD", "MbcA\\+Rv3249c\\+Rv3066", "SigK") # Needs the \\ to detect it
+# Virulence.Persistence_iModulons_pattern <- str_c(Virulence.Persistence_iModulons, collapse = "|") # Collapse all the things I am interested in into a pattern separated by or
 
 
 ###########################################################
